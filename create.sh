86829192ff40ec0409b5427aca770a5eec468d11
Hui Ren <huiren16@gmail.com>
2021-11-06 09:20:30 -0500

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
-STACK=network
-TEMPLATE=file://network.yml
-PARAMETER=file://network-parameters.json
+STACK=server
+TEMPLATE=file://server.yml
+PARAMETER=file://server-parameters.json
 REGION=us-east-1
 aws cloudformation create-stack --stack-name $STACK --template-body $TEMPLATE --parameters $PARAMETER --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$REGION
